4f858c3c87a3652b1007051095a29f17b6a12f3b
DSGQR <amini.daniel@gmail.com>
2019-04-25 19:23:45 -0700

    Add Routes

--- vidjot/app.js
+++ vidjot/app.js
@@ -2,8 +2,19 @@ const express = require('express');
 
 const app = express();
 
-const port = 5000;
+// Index Route
+app.get('/', (req, res) => {
+  console.log(req.name)
+  res.send('INDEX')
+})
 
-app.listen(port, () =>{
-  console.log(`Server started on port ${port}`);
+// About Route
+app.get('/about', (req, res) => {
+ res.send('ABOUT')
+})
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port http://localhost:${PORT}`);
 });
